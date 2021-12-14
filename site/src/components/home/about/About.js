d69b6faed1e841d3eff1508077f296ebb2d7219a
Harrison Balogh <harrisonbalogh@gmail.com>
2021-12-14 14:39:33 -0500

    resume updates

--- site/src/components/home/about/About.js
+++ site/src/components/home/about/About.js
@@ -11,17 +11,14 @@ class About extends React.Component {
         <div className="about--container-content">
             <ul className="about--profile-links" id="about--profile-links">
               <li className="github"><a href="https://github.com/harrisonbalogh" target=""><div></div></a></li>
-              <li className="facebook"><a href="https://www.facebook.com/harrison.balogh" target=""><div></div></a></li>
               <li className="linkedin"><a href="https://www.linkedin.com/in/harrison-balogh-5907a8117/" target=""><div></div></a></li>
             </ul>
             <p className="about--content-bio">
-              University taught software engineer (CS B.S.) with skills in stack
-              development (MEAN and LAMP), end-to-end encryption, agile methodology
-              with scrum workflow using JIRA, P2P networking, and graphics in numerous
-              libraries including: Swing/AWT, ThreeJS, SFML, Cocoa, Unity, SpriteKit.
-              Experience with hardware integration and structuring data measurements
-              using CGAL. This website presents some of the many projects I've worked on over
-              the years.
+              University taught software engineer with experience developing and deploying
+              full-stack web applications. Big data processing for large populations. Supporting
+              and maintaining software for medical systems. Worked with numerous graphic libraries
+              including: Swing/AWT, ThreeJS, SFML, Cocoa, Unity, Unreal, Godot, SpriteKit.
+              Experience with hardware integration on ROS for LIDAR research. This website presents some of the many personal projects I've worked on over the years.
             </p>
   
             <div className="about--navigator">
@@ -42,27 +39,25 @@ class About extends React.Component {
             <ul className="about--content-section-lang" id="about--content-section-lang-content">
               <li>
                 <p className="about--content-section-lang-title">Java</p>
-                <p className="about--content-section-lang-count">Reading projects...</p>
+                <p className="about--content-section-lang-count">See projects below...</p>
               </li>
               <li>
                 <p className="about--content-section-lang-title">Swift</p>
-                <p className="about--content-section-lang-count">Reading projects...</p>
+                <p className="about--content-section-lang-count">See projects below...</p>
               </li>
               <li>
                 <p className="about--content-section-lang-title">JavaScript</p>
-                <p className="about--content-section-lang-count">Reading projects...</p>
+                <p className="about--content-section-lang-count">See projects below...</p>
               </li>
               <li>
-                <p className="about--content-section-lang-title">HTML/CSS</p>
-                <p className="about--content-section-lang-count">Reading projects...</p>
+                <p className="about--content-section-lang-title">React</p>
+                <p className="about--content-section-lang-count">See projects below...</p>
               </li>
               <li>
                 <p className="about--content-section-lang-title">C#</p>
-                <p className="about--content-section-lang-count">Reading projects...</p>
               </li>
               <li>
                 <p className="about--content-section-lang-title">Python</p>
-                <p className="about--content-section-lang-count">Reading projects...</p>
               </li>
             </ul>
   
@@ -70,20 +65,26 @@ class About extends React.Component {
               <div className="about--content-section-icon icon-work"></div>
               <p className="about--content-section-title">Developer Experience</p>
             </div>
-            <p className="about-content-section-work-header"><b>Software Intern</b> - Tatum Games, LLC  <i>Nov. 2018-Feb. 2019</i></p>
+            <p className="about-content-section-work-header"><b>Software Engineer II</b> - Cerner Corporation  <i>Mar. 2019 - Present</i></p>
+            <p className="about-content-section-work-description">
+              Contributed to large data processing ETL pipelines; high-traffic web-based medical applications including microservices in Java and 
+              Ruby on Rails with UI contributions built in React and proprietary in-house libraries; DevOps including deployments to live client zones, 
+              performance monitoring, and support response to client and issue tracker reports.
+            </p>
+            <p className="about-content-section-work-header"><b>Software Intern</b> - Tatum Games, LLC  <i>Nov. 2018 - Feb. 2019</i></p>
             <p className="about-content-section-work-description">
               Complete sprint tasks listed in PMS on-time with team lead feedback.
               Collaborate with team members on solving any issues that may arise with
               my tasks or team members task. Recommend solutions to team members and team lead.
             </p>
-            <p className="about-content-section-work-header"><b>Research Assistant</b> - Cal State Long Beach <i>Mar. 2018-Sep. 2018</i></p>
+            <p className="about-content-section-work-header"><b>Research Assistant</b> - Cal State Long Beach <i>Mar. 2018 - Sep. 2018</i></p>
             <p className="about-content-section-work-description">
               Model autonomous robot coordination in parking garage environment. Coordinate with
               mechanical and architectural teams to design space-efficient structures for the use
               of autonomous automotive storage. Present findings on a weekly basis to research
               board and Dean of Engineering culminating in published paper.
             </p>
-            <p className="about-content-section-work-header"><b>Web Developer</b> - Freelancer <i>Mar. 2018-Sep. 2018</i></p>
+            <p className="about-content-section-work-header"><b>Web Developer</b> - Freelancer <i>Mar. 2018 - Sep. 2018</i></p>
             <p className="about-content-section-work-description" style={{marginBottom: "44px"}}>
               Build websites for clients as well as perform PHP backend administration and server
               upkeep. Clients include Claudia-Rossini, Emilio-Verdugo-Architect, TheCarriageHouseLA,
@@ -105,11 +106,11 @@ class About extends React.Component {
             </div>
             <ul className="about--content-tool-list">
               <li>Photoshop CS6</li><li>Maya</li><li>Blender</li><li>NodeJS</li>
-              <li>SpriteKit</li><li>Unity</li><li>Godot</li><li>GSAP Tween</li>
+              <li>SpriteKit</li><li>Unity</li><li>Unreal Engine</li><li>Godot</li><li>GSAP Tween</li>
               <li>JIRA</li><li>WordPress</li><li>Wix</li><li>Git</li><li>MySQL</li>
               <li>MongoDB</li><li>Xcode</li><li>Eclipse</li><li>Visual Studio</li>
               <li>Atom</li><li>NetBeans</li><li>Dreamweaver</li><li>Mac</li>
-              <li>Windows</li><li>Ubuntu</li><li>iOS</li>
+              <li>Windows</li><li>Ubuntu</li><li>iOS</li><li>Apache Suite</li>
             </ul>
           </div>
       </div>
