96525ddb5e93b6fef1f39e45e026f7e3e403d8ab
Harrison Balogh <harrisonbalogh@gmail.com>
2022-08-20 20:05:27 -0700

    section-specific sticky header heights

--- client/src/components/home/Header.js
+++ client/src/components/home/Header.js
@@ -1,11 +1,12 @@
 import './header.css'
 import React from 'react';
-import { Sections } from '../../javascript/constants';
+import HeaderSticky from './HeaderSticky';
 import projectData from '../../project_markdown/info.json'
 import PropTypes from 'prop-types';
 
-const navHeight = 40;
-const stickyHeight = 30;
+const UNKNOWN = -1
+
+const NAV_HEIGHT = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--nav-height'))
 
 const HEADER_BUTTONS = [
   'HB',
@@ -17,51 +18,57 @@ class Header extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      focusedTabIndex: 0,
-      headerButtonWidths: [],
-      stickyHeaderDisplacement: navHeight
+      iFocusedSection: 0,
+      stickyHeaderDisplacement: NAV_HEIGHT,
+      stickyHeight: UNKNOWN // Need a render frame to compute height
     }
-    this.navButtonMap = {}
   }
 
   componentDidMount() {
-    document.addEventListener('scroll', () => this.handleScroll())
+    document.addEventListener('scroll', () => this.computeDisplacement())
   }
 
-  handleScroll() {
-    let updateState = false
-    let newState = {
-      stickyHeaderDisplacement: this.state.stickyHeaderDisplacement,
-      focusedTabIndex: this.state.focusedTabIndex
+  componentDidUpdate() {
+    // Check if sticky header height was changed
+    if (this.headerSticky && this.state.stickyHeight === UNKNOWN) {
+      this.computeDisplacement(this.headerSticky.content.clientHeight)
     }
+  }
+
+  /** @param headerStickyHeight {integer} If provided, this value is used for the headerSticky height.  */
+  computeDisplacement(headerStickyHeight = undefined) {
+    let newState = {}
+
+    // Get currently focused section by compounding section heights. Retain its index
     let heightTotal = 0
-    let sectionIndex = 0
+    let iSection = 0
     for (let s = 0; s < this.props.sectionHeights.length; s++) {
       heightTotal += this.props.sectionHeights[s]
       if (window.scrollY < heightTotal) break
-      sectionIndex++
+      iSection++
     }
 
-    // Check section
-    if (this.state.focusedTabIndex !== sectionIndex) {
-      newState['focusedTabIndex'] = sectionIndex
-      updateState = true
-
-      Object.keys(this.navButtonMap).forEach(i => (this.navButtonMap[i].className = ""))
-      this.navButtonMap[Math.min(sectionIndex, Object.keys(this.navButtonMap).length - 1)].className = "focused"
-    }
-    // Check displacement (unless on last section)
-    if (sectionIndex !== this.props.sectionHeights.length - 1 && (heightTotal - stickyHeight) < window.scrollY) { // 30 is sticky header height
-        newState['stickyHeaderDisplacement'] = navHeight - (window.scrollY - (heightTotal - stickyHeight))
-        updateState = true
-    } else if (this.state.stickyHeaderDisplacement !== navHeight) {
-      newState['stickyHeaderDisplacement'] = navHeight
-      updateState = true
+    // Update focused section and clear recorded sticky height until render occurs to compute new sticky height
+    if (this.state.iFocusedSection !== iSection) {
+      newState.iFocusedSection = iSection
+      // It takes a render frame for style to compute height
+      newState.stickyHeight = UNKNOWN
+      return this.setState(newState)
     }
 
-    if (updateState) {
-      this.setState(newState)
+    let stickyHeight = headerStickyHeight === undefined ? this.state.stickyHeight : headerStickyHeight
+    if (headerStickyHeight !== undefined) {
+      newState.stickyHeight = this.headerSticky.content.clientHeight
     }
+    if (iSection !== this.props.sectionHeights.length - 1 && (heightTotal - stickyHeight) < window.scrollY) {
+      // Displace sticky header with next section
+      newState.stickyHeaderDisplacement = NAV_HEIGHT - (window.scrollY - (heightTotal - stickyHeight))
+    }
+    else if (this.state.stickyHeaderDisplacement !== NAV_HEIGHT) {
+      newState.stickyHeaderDisplacement = NAV_HEIGHT
+    }
+
+    this.setState(newState)
   }
 
   handleNavSelection(section) {
@@ -76,19 +83,17 @@ class Header extends React.Component {
     window.scrollTo(0, heightTotal)
   }
 
-  navButtons() {
+  navButtons(iFocusedSection) {
     return HEADER_BUTTONS.map((name, i) => {
-      let className = i ? '' : 'focused'
-      return <p key={i} onClick={()=>this.handleNavSelection(i)} className={className} ref={p => (this.navButtonMap[i] = p)}>
-        {name}
-      </p>
+      let className = (i === Math.min(iFocusedSection, HEADER_BUTTONS.length - 1)) ? 'focused' : ''
+      return <p key={i} onClick={()=>this.handleNavSelection(i)} className={className}>{name}</p>
     })
   }
 
   /**
-   * Conditionally displays selected project header button.
+   * Conditionally displays selected project nav button.
    */
-  selectedProjectHeaderButton(highlight) {
+  selectedProjectNavButton(highlight) {
     let className = `header-button-selected-project ${highlight ? 'highlight' : ''}`
     return (this.props.iProjectSelected !== undefined ?
       <p onClick={()=>this.handleNavSelection(3)} className={className}>
@@ -98,35 +103,19 @@ class Header extends React.Component {
     )
   }
 
-  headerStickyContent(section) {
-    let sectionContents = [
-      <p></p>,
-      <p>Harrison Balogh</p>,
-      <p>Divider</p>,
-      <p><i style={{color: 'gray'}}>Select a project...</i></p>
-    ]
-    section = Math.min(section, sectionContents.length - 1)
-    return sectionContents[section]
-  }
-
-  headerStickyClassName(section) {
-    section = Math.min(section, Sections.BY_NAME.length - 1)
-    return `nav-detail ${Sections.BY_NAME[section]}`
-  }
-
   render() {
-    const { focusedTabIndex, headerButtonWidths, stickyHeaderDisplacement} = this.state;
-    let stickyHeaderOffset = `${stickyHeaderDisplacement}px`
+    const { iFocusedSection, stickyHeaderDisplacement} = this.state;
 
     return (
-      <div className="header" ref={element => this.content = element}>
+      <div className="header">
         <div className="nav">
-          {this.navButtons()}
-          {this.selectedProjectHeaderButton(focusedTabIndex >= 3)}
+          {this.navButtons(iFocusedSection)}
+          {this.selectedProjectNavButton(iFocusedSection >= 3)}
         </div>
-        <div className={this.headerStickyClassName(focusedTabIndex)} style={{marginTop: stickyHeaderOffset}}>
-          {this.headerStickyContent(focusedTabIndex)}
-        </div>
+        <HeaderSticky ref={e => this.headerSticky = e}
+          sectionIndex={iFocusedSection}
+          displacement={stickyHeaderDisplacement}
+        />
         <div id="header-spacer-fixed-correction"></div>
       </div>
     );
--- client/src/javascript/constants.js
+++ client/src/javascript/constants.js
@@ -2,6 +2,5 @@ export const Sections = {
     BANNER: 0,
     ABOUT: 1,
     PROJECTS: 2,
-    CONTACT: 3,
-    BY_NAME: ["banner", "about", "projects-divider", 'projects', "contact"]
+    CONTACT: 3
 }
